0b278146936f7431f6e2d73e054b3cfdbf2903f2
ODS120 <odstringer@gmail.com>
2021-01-07 14:18:20 +0100

    print grid and gates

--- classes.py
+++ classes.py
@@ -1,3 +1,6 @@
+import matplotlib.pyplot as plt
+import numpy
+
 class Chip():
     def __init__(self, filename):
         self.height = 0
@@ -5,8 +8,19 @@ class Chip():
         self.coordinates = []
 
         self.load_grid(filename)
+        self.plot_chip()
         self.load_coordinates()
         self.load_gates(filename)
+
+        plt.xlim([0, self.width - 1])
+        plt.ylim([0, self.height - 1])
+        # plt.legend()
+        # plt.xticks([])
+        # plt.yticks([])
+        # plt.axis('off')
+        plt.show()
+        plt.savefig('test.png')
+
 
     # load the grid
     def load_grid(self, filename):
@@ -17,24 +31,34 @@ class Chip():
             for line in chip_grid:
                 coordinates = line.split(",")
 
-                if coordinates[1] > self.width:
-                    self.width = coordinates[1]
+                if int(coordinates[1]) > self.width:
+                    self.width = int(coordinates[1])
 
-                if coordinates[2] > self.height:
-                    self.height = coordinates[2]
+                if int(coordinates[2]) > self.height:
+                    self.height = int(coordinates[2])
             
             # add borders to the grid
             self.width += 2
             self.height += 2
+            self.coordinates = [[0 for x in range(self.width)] for y in range(self.height)]
+            print(self.coordinates)            
+
+
+    def plot_chip(self):
+        for y in range(self.height):
+            for x in range(self.width):
+                x1 = [x, x+1]
+                x2 = [x, x]
+                y1 = [y, y]
+                y2 = [y, y+1]
+                plt.plot(x1, y1, 'b', x2, y2, 'b')
 
 
     # load all the coordinate classes
     def load_coordinates(self):
         # iterate over all the grid coordinates
-        for y_coord in self.height:
-            self.coordinates[y_coord] = []
-
-            for x_coord in self.width:
+        for y_coord in range(self.height):
+            for x_coord in range(self.width):
                 coordinate = Coordinate(x_coord, y_coord)
                 self.coordinates[y_coord][x_coord] = coordinate
 
@@ -47,8 +71,14 @@ class Chip():
             for line in chip_grid:
                 gate_info = line.split(",")
                 gate_object = Gate(gate_info[0], gate_info[1], gate_info[2])
+                
+                # plot the gate onto the chip
+                gate_x = int(gate_info[1])
+                gate_y = int(gate_info[2])
+                plt.plot(gate_x, gate_y, 'ro')
+
                 # appoint the gate its position on the grid
-                self.coordinates[gate_info[1]][gate_info[2]].gate = gate_object
+                self.coordinates[gate_x][gate_y].gate = gate_object
 
 
 class Coordinate(): 
