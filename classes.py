0f33417a7b3923d147b2e50e16c1c3faad53fc5b
ODS120 <odstringer@gmail.com>
2021-01-07 17:07:52 +0100

    draw wires function, faulty

--- classes.py
+++ classes.py
@@ -1,16 +1,18 @@
 import matplotlib.pyplot as plt
-import numpy
+
 
 class Chip():
-    def __init__(self, filename):
+    def __init__(self, chip_data, netlist):
         self.height = 0
         self.width = 0
         self.coordinates = []
+        self.gates = {}
 
-        self.load_grid(filename)
+        self.load_grid(chip_data)
         self.plot_chip()
         self.load_coordinates()
-        self.load_gates(filename)
+        self.load_gates()
+        self.draw_wires(netlist)
 
         plt.xlim([0, self.width - 1])
         plt.ylim([0, self.height - 1])
@@ -23,25 +25,27 @@ class Chip():
 
 
     # load the grid
-    def load_grid(self, filename):
-        with open(filename) as chip_grid:
+    def load_grid(self, chip_data):
+        with open(chip_data) as chip_grid:
             next(chip_grid)
 
-            # find the max x and y coordinate
             for line in chip_grid:
-                coordinates = line.split(",")
+                gate_info = line.strip("\n").split(",")
+                self.gates[gate_info[0]] = {"x_coord": int(gate_info[1]), "y_coord": int(gate_info[2])}
 
-                if int(coordinates[1]) > self.width:
-                    self.width = int(coordinates[1])
+                # find the max x and y coordinate
+                if int(gate_info[1]) > self.width:
+                    self.width = int(gate_info[1])
 
-                if int(coordinates[2]) > self.height:
-                    self.height = int(coordinates[2])
+                if int(gate_info[2]) > self.height:
+                    self.height = int(gate_info[2])
             
             # add borders to the grid
             self.width += 2
             self.height += 2
-            self.coordinates = [[0 for x in range(self.width)] for y in range(self.height)]
-            print(self.coordinates)            
+
+            # fill the grid
+            self.coordinates = [[0 for x in range(self.width)] for y in range(self.height)]         
 
 
     def plot_chip(self):
@@ -64,21 +68,89 @@ class Chip():
 
 
     # load all the gate classes
-    def load_gates(self, filename):
-        with open(filename) as chip_grid:
-            next(chip_grid)
+    def load_gates(self):
+        for gate_id in self.gates:
+            gate = self.gates[gate_id]
+            gate_object = Gate(gate_id, gate["x_coord"], gate["y_coord"])
 
-            for line in chip_grid:
-                gate_info = line.split(",")
-                gate_object = Gate(gate_info[0], gate_info[1], gate_info[2])
-                
-                # plot the gate onto the chip
-                gate_x = int(gate_info[1])
-                gate_y = int(gate_info[2])
-                plt.plot(gate_x, gate_y, 'ro')
+            # plot the gate onto the chip
+            plt.plot(gate["x_coord"], gate["y_coord"], 'ro', marker = "s", markersize = 20)
 
-                # appoint the gate its position on the grid
-                self.coordinates[gate_x][gate_y].gate = gate_object
+            # plot gate label 
+            plt.annotate(f"{gate_id}", (gate["x_coord"], gate["y_coord"]), fontsize = 13, ha = "center", va = "center")
+
+            # appoint the gate its position on the grid
+            self.coordinates[gate["x_coord"]][gate["y_coord"]].gate = gate_object
+
+
+    def draw_wires(self, file):
+        # with open(file) as netlist:
+        #     next(netlist)
+        gate_a = self.gates["2"]
+        gate_b = self.gates["3"]
+        draw_x = gate_a["x_coord"]
+        draw_y = gate_a["y_coord"]
+        
+        
+        while draw_x != gate_b["x_coord"]:
+            # east
+            if draw_x < gate_b["x_coord"]:
+                pass
+            # west
+            if draw_x > gate_b["x_coord"]:
+                pass
+        
+
+
+        
+        while draw_y > gate_b["y_coord"]:
+            x1 = [draw_x, draw_x]
+            y1 = [draw_y, draw_y - 1]
+            plt.plot(x1, y1, 'r')
+            draw_y -= 1
+
+    def wire_north(self, draw_x, draw_y):
+        self.coordinates[draw_x][draw_y].connections.append("north")
+        self.coordinates[draw_x][draw_y + 1].connections.append("south")
+
+        x1 = [draw_x, draw_x]
+        y1 = [draw_y, draw_y + 1]
+    
+        plt.plot(x1, y1, 'r')
+        draw_y += 1
+
+    def wire_east(self, draw_x, draw_y):
+        self.coordinates[draw_x][draw_y].connections.append("east")
+        self.coordinates[draw_x + 1][draw_y].connections.append("west")
+
+        x1 = [draw_x, draw_x]
+        y1 = [draw_y, draw_y]
+    
+        plt.plot(x1, y1, 'r')
+        draw_x += 1
+    
+    def wire_south(self, draw_x, draw_y):
+        self.coordinates[draw_x][draw_y].connections.append("south")
+        self.coordinates[draw_x - 1][draw_y].connections.append("north")
+
+        x1 = [draw_x, draw_x]
+        y1 = [draw_y, draw_y - 1]
+        
+        plt.plot(x1, y1, 'r')
+        draw_y -= 1
+        
+    def wire_west(self, draw_x, draw_y):
+        self.coordinates[draw_x][draw_y].connections.append("west")
+        self.coordinates[draw_x - 1][draw_y].connections.append("east")
+        print(self.coordinates[5][5].connections)
+        x1 = [draw_x, draw_x - 1]
+        y1 = [draw_y, draw_y]
+        
+        plt.plot(x1, y1, 'r')
+        draw_x -= 1
+    
+
+
 
 
 class Coordinate(): 
--- main.py
+++ main.py
@@ -5,36 +5,12 @@ from classes import Chip, Coordinate, Gate
 
 if __name__ == "__main__":
     # check command line arguments
-    if len(argv) != 2:
+    if len(argv) != 3:
         print("Usage: 'python main.py [print_x] [netlist_y]")
         exit(1)
-    # create chip object from 'print data'
-    chip = Chip(argv[1])
+        
+    # create chip object from files
+ 
+    chip = Chip(argv[1], argv[2])
 
-    # visualize grid
-    # width = 6
-    # height = 6
-    # for y in range(height):
-    #     for x in range(width):
-    #         x1 = [x, x+1]
-    #         x2 = [x, x]
-    #         y1 = [y, y]
-    #         y2 = [y, y+1]
-    #         plt.plot(x1, y1, 'b', x2, y2, 'b')
-    # plt.plot(x1, y1, 'b', x2, y2, 'b')
-    # x3 = [3]
-    # y3 = [3]
-    # plt.plot(x3, y3, 'ro')
-
-
-
-    # plt.xlim([0, 5])
-    # plt.ylim([5, 0])
-    # plt.xlim([0, chip.width()])
-    # plt.ylim([chip.height(), 0])
-    # # plt.legend()
-    # # plt.xticks([])
-    # # plt.yticks([])
-    # plt.axis('off')
-    # plt.show()
-    # plt.savefig('test.png')
+    
